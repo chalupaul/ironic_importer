27b8710e99aa2891fb11ab9dd1015960c39733d0
chalupaul <me@chalupaul.com>
2019-07-09 17:49:54 -0500

    updated packaging for pypi

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
+"""
+   Copyright [2019] [Paul Sims]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
 from setuptools import setup, find_packages
 from os import path
 
@@ -35,6 +51,7 @@ setup(
             'python-novaclient==13.0.1',
             'python-glanceclient==2.16.0',
             ],
+        data_files=[('examples', ['examples/ironic_nodes.xlsx'])],
         entry_points={
             'console_scripts': [
                 'ironic_inventory=ironic_importer.inventory:main',
